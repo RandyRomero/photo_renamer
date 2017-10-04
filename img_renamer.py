60fbe5bf1768ccbb7c93117205bfe5a0f748566d
RandyRomero <a.ya.miheev@gmail.com>
2017-10-04 11:13:04 +0300

    Work_with_exif__data is no more nested function

--- img_renamer.py
+++ img_renamer.py
@@ -7,51 +7,12 @@ import os
 import shutil
 import exifread
 
-image_extension = ('.jpg', '.jpeg')
-
 
 def process_files(path_with_images):
-
-    def work_with_exif_data(exif, picture):
-        images_with_info = []  # List of all images with their info from exif
-        to_print = ''  # All info about one particular image in string format
-
-        date_time = str(exif.get('EXIF DateTimeOriginal', None))  # Get date when picture was shot
-
-        if date_time == 'None':  # If there is no date and time - exit function
-            print('There is no EXIF date')
-            return
-
-        camera_brand = str(exif.get('Image Make', None))
-        camera_model = str(exif.get('Image Model', None))
-        lens_brand = str(exif.get('EXIF LensMake', None))
-        lens_model = str(exif.get('EXIF LensModel', None))
-
-        # No body cares whether Nikon is corporation of whatever
-        if camera_brand == 'NIKON CORPORATION':
-            camera_brand = 'NIKON'
-
-        # If camera brand is also denoted in camera model - get rid of camera brand in camera model
-        if camera_brand in camera_model:
-            camera_model = (camera_model.replace(camera_brand, ''))[1:]
-
-        # Add entries in list if entry is not empty (contains zero in our case)
-        one_image_with_info = []
-        for entry in [picture, date_time, camera_brand, camera_model, lens_brand, lens_model]:
-            if entry != 'None':
-                one_image_with_info.append(entry)
-            # Add list with path to image and with image's info into one big list
-            images_with_info.append(one_image_with_info)
-
-        # Prepare every entry to be printed out
-        for item in images_with_info:
-            for i in item:
-                to_print += i + ' '
-                to_print = to_print.replace(':', '.')
-                to_print = to_print.replace('/', '')
-        print(to_print)
-
     # Search for photos, open them and extract exif info
+
+    image_extension = ('.jpg', '.jpeg')
+
     for root, subfolders, files in os.walk(path_with_images):
         for file in files:
             if file.lower().endswith(image_extension):
@@ -60,11 +21,47 @@ def process_files(path_with_images):
                     image = os.path.join(root, file)
                     work_with_exif_data(tags, image)
 
-    # for image in images_with_info:
-    #     image_name = (image['Image_date_time'] + ' ' + image['Camera'] + ' ' + image['Lens'])
-    #     image_name = image_name.replace(':', '.')
-    #     image_name = image_name.replace('/', '')
-    #     print(image_name)
+
+def work_with_exif_data(exif, picture):
+    # Gather info about every image and store all in list
+
+    one_image_with_info = []  # All info about image in list form
+    images_with_info = []  # List of all images with their info from exif
+    to_print = ''  # All info about one particular image in string format
+
+    date_time = str(exif.get('EXIF DateTimeOriginal', None))  # Get date when picture was shot
+
+    if date_time == 'None':  # If there is no date and time - exit function
+        print('There is no EXIF date')
+        return
+
+    camera_brand = str(exif.get('Image Make', None))
+    camera_model = str(exif.get('Image Model', None))
+    lens_brand = str(exif.get('EXIF LensMake', None))
+    lens_model = str(exif.get('EXIF LensModel', None))
+
+    # No body cares whether Nikon is corporation of whatever
+    if camera_brand == 'NIKON CORPORATION':
+        camera_brand = 'NIKON'
+
+    # If camera brand is also denoted in camera model - get rid of camera brand in camera model
+    if camera_brand in camera_model:
+        camera_model = (camera_model.replace(camera_brand, ''))[1:]
+
+    # Add entries in list if entry is not empty (contains zero in our case)
+    for entry in [picture, date_time, camera_brand, camera_model, lens_brand, lens_model]:
+        if entry != 'None':
+            one_image_with_info.append(entry)
+        # Add list with path to image and with image's info into one big list
+    images_with_info.append(one_image_with_info)
+
+    # Prepare every entry to be printed out
+    for item in images_with_info:
+        for i in item:
+            to_print += i + ' '
+            to_print = to_print.replace(':', '.')
+            to_print = to_print.replace('/', '')
+    print(to_print)
 
 while True:
     path_to_look_for_photos = input('Please type in directory with your photos:\n')
