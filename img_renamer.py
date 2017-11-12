ebb5ddaad6c123dce30761134dbc5a5256926481
RandyRodriguez <a.ya.miheev@gmail.com>
2017-11-12 16:56:26 +0300

    make additional rules to avoid name collision, superfluous copies etc, but it doesnt work so far

--- img_renamer.py
+++ img_renamer.py
@@ -7,12 +7,15 @@ import os
 import shelve
 import exifread  # library to get exif data from file
 import handle_logs
+from send2trash import send2trash
+import sys
 
 logFile, logConsole = handle_logs.set_loggers()
 handle_logs.clean_log_folder(20, logFile, logConsole)
 logFile.info('Program has started')
 
 images_with_info = []  # List of all images with their info from exif
+images_to_delete = []  # list of superfluous copies to remove
 name_strings = []
 perm_denied_files = []
 
@@ -29,8 +32,8 @@ def process_files(path_with_images):
                     # details=False to avoid extracting superfluous data from EXIF and overflowing memory
                     tags = exifread.process_file(f, details=False)
                     path_to_image = os.path.join(root, file)
-                    data = work_with_exif_data(tags, path_to_image)
-                    if data:
+                    data = work_with_exif_data(tags, path_to_image, file)
+                    if data != -1:
                         images_with_info.append(data)
 
 
@@ -45,11 +48,12 @@ def open_db():
     return shelve_db
 
 
-def work_with_exif_data(exif, path_to_picture):
+def work_with_exif_data(exif, path_to_picture, file):
 
     """
     Gather info about image and make a string with it
 
+    :param file: of file to compare with new name to avoid creating of copies of the same files
     :param exif: full exif data from current file
     :param path_to_picture: full path to picture
     :return: list where first item is path to picture and the second is string with new name for picture
@@ -107,27 +111,57 @@ def work_with_exif_data(exif, path_to_picture):
         # Convert back to string from list and return
         return ' '.join(words_object)
 
-    def check_duplicates(string):
-        if string not in name_strings:
-            name_strings.append(string)
+    def check_duplicates(supposed_name, counter):
+        # Function checks whether file is going to get unique name after renaming.
+        # If there is more than one file with this name (usually because of burst shooting) these file should
+        # be named as someName[2].jpg, someName[3].jpg and so on. Function just keeps track of every name that app
+        # is going to give to every file.
+
+        if supposed_name not in name_strings:
+            name_strings.append(supposed_name)
+            return supposed_name
         else:
             print('DUPLICATE')
-            counter = 2
-            while string + ' ({})'.format(counter) in name_strings:
+            if counter is None:
+                counter = 2
+            while supposed_name + '[{}]'.format(counter) in name_strings:
                 counter += 1
-            string = string + ' ({})'.format(counter)
-            name_strings.append(string)
+            final_name = supposed_name + '[{}]'.format(counter)
+            name_strings.append(final_name)
 
-        return string
+        return final_name
 
+    def binary_comparison(existing_file, original_picture_path):
+        # Explanation below in message_text
+        with open(existing_file, 'rb') as a:
+            with open(original_picture_path, 'rb') as b:
+                # logConsole.debug(existing_file)
+                # logConsole.debug(original_picture_path)
+                if a.read() == b.read():
+                    message_text = 'There is already this photo named \'' + existing_copy + '.jpg\' in this folder.' \
+                                    '\nOld copy will be transferred to trash bin.\n'
+                    print(message_text)
+                    logFile.info(message_text)
+                    # logConsole.debug(os.path.exists(path_to_picture))
+                    images_to_delete.append(original_picture_path)
+                    # print(path_to_picture + ' was removed.')
+                    # logFile.info(path_to_picture + ' was removed.')
+                    return True
+                else:
+                    return False
+                    # logConsole.error('I am not sure that it is possible. So will test and see')
+                    # logFile.error('I am not sure that it is possible. So will test and see')
+                    # db.close()
+                    # sys.exit()
+
+    original_filename = file
     one_image_with_info = []  # All info about image in list form
-
     date_time = str(exif.get('EXIF DateTimeOriginal', None))  # Get date when picture was shot
 
     if date_time == 'None':  # If there is no date and time - exit function
         print(path_to_picture + ' --- there is no EXIF data.\n')
         logFile.info(path_to_picture + ' --- there is no EXIF data.\n')
-        return
+        return -1
 
     camera_brand = str(exif.get('Image Make')).strip()
     camera_model = str(exif.get('Image Model')).strip()
@@ -165,13 +199,73 @@ def work_with_exif_data(exif, path_to_picture):
     # Replace not allowed characters before calling function
     name_string = remove_repeated_words(name_string.replace(':', '-').replace('/', ''))
     name_string = name_string[:-1]
-    name_string = check_duplicates(name_string)
+
+    logConsole.debug(path_to_picture)
+    if path_to_picture == r'C:\ya.disk\YandexDisk\Photo_2017\[01] January\2017-01-01 01-08-55 Xiaomi Redmi Note 3 Pro (2).jpg':
+        logFile.debug('how this file is not on folder?')
+        logConsole.debug('how this file is not on folder?')
+    # logConsole.debug(os.path.join('\\'.join(path_to_picture.split('\\')[:-1]), name_string + '.jpg'))
+
+    if (name_string + '.jpg') == original_filename:
+        # File already has exactly this name
+        print('New name matches actual name. This file has already been renamed.\n')
+        logFile.warning('New name matches actual name. This file has already been renamed.\n')
+        return -1
+
+    # Check folder whether there is already a file with name that script wants give to picture
+    if os.path.exists(os.path.join('\\'.join(path_to_picture.split('\\')[:-1]), name_string + '.jpg')):
+        existing_copy = os.path.join('\\'.join(path_to_picture.split('\\')[:-1]), name_string)
+        # Check if already existed files and file in process are the same files by binary comparison
+        if binary_comparison(existing_copy + '.jpg', path_to_picture):
+            return -1
+        counter = 2
+        # If they are not the same, that check maybe there is a file with [2].jpg or [3].jpg etc
+        # logConsole.debug(existing_copy + '[{}].jpg'.format(counter))
+
+        while os.path.exists(existing_copy + '[{}].jpg'.format(counter)):
+            # if there is file with [counter].jpg at the end, check maybe it are exact duplicate of file on process
+            logFile.debug('File \'' + existing_copy + '[{}].jpg\' already exists in this folder.'.format(counter))
+            print('File \'' + existing_copy + '[{}].jpg\' already exists in this folder.'.format(counter))
+            if existing_copy + '[{}].jpg'.format(counter) == path_to_picture:
+                # If now it turns out existing file have the same name, skip renaming and start over with next file
+                print('New name matches actual name. This file has already been renamed.\n')
+                logFile.warning('New name matches actual name. This file has already been renamed.\n')
+                return -1
+            if binary_comparison(existing_copy + '[{}].jpg'.format(counter), path_to_picture):
+                # If yes, skip this file and start over with next one
+                return -1
+            counter += 1
+
+        # name_string = check_duplicates(existing_copy, counter)
+        name_string = existing_copy + '[{}]'.format(counter)
+
+    else:
+        name_string = check_duplicates(name_string)
+
     one_image_with_info.extend([path_to_picture, name_string])
+
+    # elif os.path.exists(os.path.join('\\'.join(path_to_picture.split('\\')[:-1]), name_string)):
+    #     # Check if there is already file with this name in folder
+    #     # If yes and if they are exact duplicates then remove not renamed file
+    #     existing_copy = os.path.join('\\'.join(path_to_picture.split('\\')[:-1]), name_string)
+    #     handle_existing_copies(existing_copy)
+    # logConsole.debug(one_image_with_info[1] + '.jpg' + ' vs ' + original_filename)  # just for one test
     print('How it will be renamed: ')
     print(one_image_with_info[1] + '.jpg\n')
     logFile.info('How it will be renamed: ')
     logFile.info(one_image_with_info[1] + '.jpg\n')
     return one_image_with_info
+
+
+def remove_copies():
+    print('Start to remove superfluous copies.')
+    logFile.info('Start to remove superfluous copies.')
+    for file in images_to_delete:
+        send2trash(file)
+        print(file + ' was removed.')
+        logFile.info(file + ' was removed.')
+    print('All old copies were removed')
+    logFile.info('All old copies were removed')
 
 
 def rename_photos():
@@ -207,7 +301,9 @@ while True:
         db.close()
         logFile.info('Database was closed successfully')
         print('There are ' + str(len(images_with_info)) + ' files to rename.')
-        logFile.info('There are ' + str(len(images_with_info)) + ' files to rename.\n')
+        logFile.info('There are ' + str(len(images_with_info)) + ' files to rename.')
+        print('There are ' + str(len(images_to_delete)) + ' old copies to delete.')
+        logFile.info('There are ' + str(len(images_to_delete)) + ' old copies to delete.')
         break
     else:
         print('This path doesn\'t exist. Try another one')
@@ -221,6 +317,21 @@ if len(images_with_info) > 0:
             rename_photos()
             break
         elif rename_or_not.lower() == 'n':
+            print('Ciao!')
+            logFile.info('Ciao!\n')
+            break
+        else:
+            print('It is wrong input, try again.')
+            logFile.info('It is wrong input, try again.\n')
+
+if len(images_to_delete) > 0:
+    while True:
+        delete_or_not = input('Do you want to delete old copies? y/n: ')
+        logFile.info('Do you want to delete old copies? y/n: \n')
+        if delete_or_not.lower() == 'y':
+            remove_copies()
+            break
+        elif delete_or_not.lower() == 'n':
             print('Ciao!')
             logFile.info('Ciao!\n')
             break
---
+++ sendToTrashChecker.py
@@ -0,0 +1,3 @@
+from send2trash import send2trash
+
+send2trash(r'C:\\Users\\ayami\\Desktop\\Новая папка\\2017-08-01 23-17-05.JPG')
