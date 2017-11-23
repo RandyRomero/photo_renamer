10a71e3b7c856c143e46ccc32f7f8acf5a1d3fa2
RandyRomero <a.ya.miheev@gmail.com>
2017-11-23 09:42:51 +0300

    Update comments

--- photo_renamer.py
+++ photo_renamer.py
@@ -5,14 +5,14 @@
 Written by Aleksandr Mikheev
 https://github.com/RandyRomero/photo_renamer/
 
-A little script that reads data from exif from your jpg files and rename it accodringly.
+A little script that reads data from exif from your jpg files and rename it accordingly.
 First it asks folder to look through for your jpg files. Important note: script goes through subfolders as well.
 Then it reads exif info from jpg files and give you two lists to check: one is a list of files to be renamed,
 second is a list of files to be deleted.
 It will rename files like this "yyyy-mm-dd hh-mm-ss camerabrand cameramodel lensbrand lensmodel".
 In case there are several files with the same date of shooting,
 script will add [next number] to the end of the file name.
-Script can also detect duplicates with other names and that's really usefull sometimes.
+Script can also detect duplicates with other names and that's really useful sometimes.
 It will offer you to remove them.
 
 It was developed and tested under Windows 10 64 bit with Python 3.6.2.
@@ -21,7 +21,7 @@ It was developed and tested under Windows 10 64 bit with Python 3.6.2.
 import os
 import shelve
 import exifread  # library to get exif data from file
-import handle_logs # separate file for setting up logging to console and log file
+import handle_logs  # separate file for setting up logging to console and log file
 from send2trash import send2trash
 
 # setting up loggers
@@ -31,8 +31,8 @@ logFile.info('Program has started')
 
 images_with_info = []  # List of all images with their info from exif
 images_to_delete = []  # list of superfluous copies to remove
-name_strings = {} # Dict where key is a final new name of a photo and values is a full path to this photo
-perm_denied_files = [] # Files that script could not rename because it was locked by OS
+name_strings = {}  # Dict where key is a final new name of a photo and values is a full path to this photo
+perm_denied_files = []  # Files that script could not rename because it was locked by OS
 
 
 def process_files(path_with_images):
@@ -53,7 +53,7 @@ def process_files(path_with_images):
 
 
 def open_db():
-    # Open database which contains informations how different tags from exif rename to normal names
+    # Open database which contains information how different tags from exif rename to normal names
     # e.g 'NIKON CORPORATION' to 'Nikon' or 'chiron' to 'Mi MIx 2'
     if not os.path.exists('db'):
         os.mkdir('db')
@@ -167,7 +167,7 @@ def get_new_name_for_photo(exif, path_to_picture, file):
 
         :param supposed_name: string with name that script wants to give to a photo
         :return: it either returns new name according to existing duplicates (is any) or returns None if current photo
-        ia a duplicate or has been salready renamed
+        ia a duplicate or has been already renamed
 
         """
 
@@ -176,7 +176,12 @@ def get_new_name_for_photo(exif, path_to_picture, file):
         counter = 1
 
         def already_has_this_name(name):
-            # Check whether file already has exactly this name that script wants to give it
+            """
+            Check whether file already has exactly this name that script wants to give it
+            :param name: string with new supposed name of file
+            :return: Boolean
+            """
+
             if name.lower() == original_filename.lower():
                 print('New name matches current name. This file has already been renamed.')
                 logFile.info('New name matches current name. This file has already been renamed.')
@@ -184,8 +189,14 @@ def get_new_name_for_photo(exif, path_to_picture, file):
             return False
 
         def get_new_order_name(name, name_with_counter):
-            # Avoid giving the same names for photos that were taken during the same second
-            # Also avoid giving new order names to duplicates instead of ignoring them
+            """
+            Avoid giving the same names for photos that were taken during the same second
+            Also avoid giving new order names to duplicates instead of ignoring them
+            :param name: string with new supposed name of file
+            :param name_with_counter: same name, but with counter to manage photos with same date of shooting
+            :return: string with name with counter or None
+            """
+
             nonlocal counter
 
             # We don't always need to use name_with_counter in this function,
@@ -195,10 +206,12 @@ def get_new_name_for_photo(exif, path_to_picture, file):
 
             print('That name has already been picked up during this session.')
             logFile.info('That name has already been picked up during this session.')
+            # Check whether files are duplicates
             if binary_comparison(path_to_picture, name_strings[name_with_counter]):
                 return None
 
             # Check if it is possible to give to file a name with next order number
+            # or name with this order number has been already picked up during the session
             counter += 1
             while name + '[{}]'.format(counter) in list(name_strings.keys()):
                 logFile.info('New supposed name is "' + name + '[{}].jpg"'.format(counter))
@@ -234,7 +247,7 @@ def get_new_name_for_photo(exif, path_to_picture, file):
                 counter = 2
                 # Try to give it new name with next order number
                 while os.path.exists(os.path.join(path_to_existing_copy, supposed_name + '[{}].jpg'.format(counter))):
-                    # Maybe there is already file with this name with order number
+                    # Check if file already has this name and script doesn't need to rename it
                     if already_has_this_name(supposed_name + '[{}].jpg'.format(counter)):
                         return None
                     new_supposed_name = os.path.join(path_to_existing_copy, supposed_name + '[{}].jpg'.format(counter))
@@ -245,7 +258,7 @@ def get_new_name_for_photo(exif, path_to_picture, file):
                         return None
                     counter += 1
 
-                # Second check if supposed name hasn't been picked up already
+                # Second check whether supposed name has been picked up already (yes, it is 100% need second check)
                 if supposed_name + '[{}]'.format(counter) in list(name_strings.keys()):
                     supposed_name = get_new_order_name(supposed_name, supposed_name + '[{}]'.format(counter))
                     if not supposed_name:
@@ -266,12 +279,13 @@ def get_new_name_for_photo(exif, path_to_picture, file):
         logFile.info(path_to_picture + ' --- there is no EXIF data.\n')
         return -1
 
+    # Get necessary tags from EXIF data
     camera_brand = str(exif.get('Image Make')).strip()
     camera_model = str(exif.get('Image Model')).strip()
     lens_brand = str(exif.get('EXIF LensMake')).strip()
     lens_model = str(exif.get('EXIF LensModel')).strip()
 
-    # Show raw data from exif
+    # Show to user raw data from EXIF
     print('\nRaw data from ' + path_to_picture)
     logFile.info('')
     logFile.info('Raw data from ' + path_to_picture)
@@ -280,6 +294,7 @@ def get_new_name_for_photo(exif, path_to_picture, file):
     logFile.info('DateTime: {} Camera brand: {} Camera model: {} Lens brand: {} Lens model: {}'
                  .format(date_time, camera_brand, camera_model, lens_brand, lens_model))
 
+    # Check if we have more appropriate name in database for every tag
     if camera_brand != 'None':
         camera_brand = check_tag(camera_brand, 'camera_brand')
 
@@ -312,6 +327,8 @@ def get_new_name_for_photo(exif, path_to_picture, file):
 
     one_image_with_info.extend([path_to_picture, name_string])
 
+    # Put final file name and it's full path in dictionary in order to be able to keep tracked of name that have been
+    # picked up during the session and to be able to perform binary comparison to figure out duplicates
     name_strings[name_string] = path_to_picture
     print('How it will be renamed: ')
     print(one_image_with_info[1] + '.jpg\n')
@@ -321,6 +338,10 @@ def get_new_name_for_photo(exif, path_to_picture, file):
 
 
 def remove_copies():
+    """
+    Function for recursive removing files from list
+    :return: doesn't get or return anything
+    """
     print('Start to remove superfluous copies.')
     logFile.info('Start to remove superfluous copies.')
     for file in images_to_delete:
@@ -332,8 +353,12 @@ def remove_copies():
 
 
 def rename_photos():
+    """
+    Recursively rename photos
+    :return: doesn't get or return anything
+    """
     for item in images_with_info:
-        # Remove name of file from full path to file
+        # Remove current name of file from full path to file and add new name to path
         new_name = os.path.join('\\'.join(item[0].split('\\')[:-1]), item[1])
 
         if os.path.exists(new_name + '.jpg'):
@@ -352,6 +377,7 @@ def rename_photos():
 print('Hello! This script can help you to automatically rename your photos (jpg files) from whatever name they have to'
       ' name like Date and time of creation + camera nad lens. For example: '
       '2017-09-05 09-15-27 Canon EOS 80D EF-S24mm f2.8 STM.jpg')
+
 
 while True:
     path_to_look_for_photos = input('Please type in directory with your photos:\n')
